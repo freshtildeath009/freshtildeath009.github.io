14a5b881bf135dac6ba0facdc010baee10735127
freshtildeath009 <visrobis@gmail.com>
2023-09-05 20:51:32 +0800

    Add images slider to projects

---
+++ data.js
@@ -0,0 +1,8 @@
+const imagesSlide = [
+    './assets/.images/ecomnmerce1',
+    './assets/.images/ecomnmerce1',
+    './assets/.images/ecomnmerce1'
+]
+imagesSlide.forEach((images)=>{
+    console.log(images);
+})
